36569b7ce67298071b969130832871d26c62905b
Wonmo Jung <jungcro@gmail.com>
2021-10-06 21:02:52 +0900

    export library

--- tlwr_analytics/tlwr_observer/lib/tlwr_observer.dart
+++ tlwr_analytics/tlwr_observer/lib/tlwr_observer.dart
@@ -1,7 +1,4 @@
 library tlwr_observer;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'package:tlwr_observer/tlwr_observer.dart';
+export 'tlwr_page_widget/tlwr_page_widget.dart' show TLWRPageWidget;
