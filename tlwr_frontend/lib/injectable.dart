109b2776f8f079f62df40a2e21c8c6ae950abc50
Aqudi <gjdigj145@gmail.com>
2021-10-08 04:24:00 +0900

    integrate with injectable and add theme datas

--- tlwr_frontend/lib/injectable.dart
+++ tlwr_frontend/lib/injectable.dart
@@ -1,12 +1,10 @@
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:tlwr_frontend/injectable.config.dart';
 
 final GetIt getIt = GetIt.instance;
 
 @injectableInit
 void configureInjection(String env) {
-  getIt.registerSingleton(Supabase.instance);
   $initGetIt(getIt, environment: env);
 }
--- tlwr_frontend/lib/presentation/core/app_widget.dart
+++ tlwr_frontend/lib/presentation/core/app_widget.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tlwr_frontend/application/auth/auth_bloc.dart';
+import 'package:tlwr_frontend/injectable.dart';
 
 import 'package:tlwr_frontend/presentation/routes/router.gr.dart';
 import 'package:tlwr_frontend/presentation/shared/colors.dart';
@@ -9,16 +12,89 @@ class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final _appRouter = AppRouter();
+    final defaultTextTheme = Typography.material2018().white;
 
-    return MaterialApp.router(
-      title: 'TLWR: The Long and Winding Road',
-      routeInformationProvider: _appRouter.routeInfoProvider(),
-      routerDelegate: _appRouter.delegate(),
-      routeInformationParser: _appRouter.defaultRouteParser(),
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        primaryColorDark: kcPrimaryColor,
-        backgroundColor: kcBackgroundColor,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) =>
+              getIt<AuthBloc>()..add(const AuthEvent.authCheckRequested()),
+        )
+      ],
+      child: MaterialApp.router(
+        title: 'TLWR: The Long and Winding Road',
+        routeInformationProvider: _appRouter.routeInfoProvider(),
+        routerDelegate: _appRouter.delegate(),
+        routeInformationParser: _appRouter.defaultRouteParser(),
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          primaryColor: kcPrimaryColor,
+          primarySwatch: kcPrimarySwatch,
+          backgroundColor: kcBackgroundColor,
+          scaffoldBackgroundColor: kcBackgroundColor,
+          textTheme: TextTheme(
+            headline1: defaultTextTheme.headline1?.copyWith(
+              inherit: false,
+              fontSize: 48,
+              fontWeight: FontWeight.w500,
+              textBaseline: TextBaseline.alphabetic,
+            ),
+            headline2: defaultTextTheme.headline2?.copyWith(
+              inherit: false,
+              fontSize: 34,
+              fontWeight: FontWeight.w500,
+              textBaseline: TextBaseline.alphabetic,
+            ),
+            headline3: defaultTextTheme.headline3?.copyWith(
+              inherit: false,
+              fontSize: 24,
+              fontWeight: FontWeight.w500,
+              textBaseline: TextBaseline.alphabetic,
+            ),
+            bodyText1: defaultTextTheme.bodyText1?.copyWith(
+              inherit: false,
+              fontSize: 16,
+              fontWeight: FontWeight.w500,
+              textBaseline: TextBaseline.alphabetic,
+            ),
+            bodyText2: defaultTextTheme.bodyText2?.copyWith(
+              inherit: false,
+              fontSize: 14,
+              fontWeight: FontWeight.w400,
+              textBaseline: TextBaseline.alphabetic,
+            ),
+            subtitle1: defaultTextTheme.subtitle1?.copyWith(
+              inherit: false,
+              fontSize: 24,
+              fontWeight: FontWeight.w400,
+              textBaseline: TextBaseline.alphabetic,
+            ),
+            subtitle2: defaultTextTheme.subtitle2?.copyWith(
+              inherit: false,
+              fontSize: 20,
+              fontWeight: FontWeight.w500,
+              textBaseline: TextBaseline.alphabetic,
+            ),
+            caption: defaultTextTheme.caption?.copyWith(
+              inherit: false,
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              textBaseline: TextBaseline.alphabetic,
+            ),
+            button: defaultTextTheme.button?.copyWith(
+              inherit: false,
+              fontSize: 14,
+              fontWeight: FontWeight.w500,
+              textBaseline: TextBaseline.alphabetic,
+            ),
+            overline: defaultTextTheme.overline?.copyWith(
+              inherit: false,
+              fontSize: 10,
+              fontWeight: FontWeight.w400,
+              textBaseline: TextBaseline.alphabetic,
+            ),
+          ),
+        ),
       ),
     );
   }
