e36b9cb6e8efabd7f67c93e5611ee4f1ef9dcbe0
Aqudi <gjdigj145@gmail.com>
2021-10-10 05:43:22 +0900

    fix tlwr_graph_viz data model key

--- tlwr_graph_viz/lib/data_models/dynamic_event.dart
+++ tlwr_graph_viz/lib/data_models/dynamic_event.dart
@@ -9,8 +9,10 @@ class DynamicEvent with _$DynamicEvent {
     required String id,
     // ignore: invalid_annotation_target
     @JsonKey(name: 'created_at') String? createdAt,
-    required String fromNodeId,
-    required String toNodeId,
+    // ignore: invalid_annotation_target
+    @JsonKey(name: 'from') required String fromNodeId,
+    // ignore: invalid_annotation_target
+    @JsonKey(name: 'to') required String toNodeId,
   }) = _DynamicEvent;
 
   factory DynamicEvent.fromJson(Map<String, dynamic> json) =>
