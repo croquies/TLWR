da00c6997ef9c7521d0e06e7ec2439ef6b357c1f
Aqudi <gjdigj145@gmail.com>
2021-10-11 11:31:47 +0900

    Deploy frontend

--- tlwr_frontend/lib/presentation/shared/resources/images.dart
+++ tlwr_frontend/lib/presentation/shared/resources/images.dart
@@ -3,7 +3,7 @@ part of 'resources.dart';
 class Images {
   Images._();
 
-  // static const String logo = 'assets/images/logo.png';
-  static const String logo = 'assets/images/tlwr_logo.png';
+  static const String logo = 'assets/images/logo.png';
+  // static const String logo = 'assets/images/tlwr_logo.png';
   static const String tlwrImage = 'assets/images/tlwr.png';
 }
